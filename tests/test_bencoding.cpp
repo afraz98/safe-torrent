0f5777ca811ce6f0b55dc52469e530c0ea15af70
afraz98 <anthonymfrazier1998@gmail.com>
2023-04-09 23:35:51 -0400

    ...

--- tests/test_bencoding.cpp
+++ tests/test_bencoding.cpp
@@ -1,40 +1,42 @@
 #include <gtest/gtest.h>
-#include "bencoding.cpp"
+#include "bencoding.h"
 
-class BencodeParserTest : public ::testing::Test{
-    TEST(BencodeParserTest, ParseStringTest) {
-        Bencoding::BencodeParser parser;
-        std::string input = "5:hello";
-        std::string expected_output = "hello";
-        std::istringstream iss(input);
-        parser.setInput(iss);
-        std::string output = parser.parseString();
-        EXPECT_EQ(output, expected_output);
-    }
+class BencodeParserTest : public ::testing::Test 
+{
+};
 
-    TEST(BencodeParserTest, ParseStringEmptyInputTest) {
-        Bencoding::BencodeParser parser;
-        std::string input = "0:";
-        std::string expected_output = "";
-        std::istringstream iss(input);
-        parser.setInput(iss);
-        std::string output = parser.parseString();
-        EXPECT_EQ(output, expected_output);
-    }
+TEST_F(BencodeParserTest, ParseStringTest) {
+    Bencoding::BencodeParser parser;
+    std::string input = "5:hello";
+    std::string expected_output = "hello";
+    std::istringstream iss(input);
+    parser.setInput(iss);
+    std::string output = parser.parseString();
+    EXPECT_EQ(output, expected_output);
+}
 
-    TEST(BencodeParserTest, ParseStringInvalidInputTest) {
-        Bencoding::BencodeParser parser;
-        std::string input = "5hello";
-        std::istringstream iss(input);
-        parser.setInput(iss);
-        EXPECT_THROW(parser.parseString(), std::invalid_argument);
-    }
+TEST_F(BencodeParserTest, ParseStringEmptyInputTest) {
+    Bencoding::BencodeParser parser;
+    std::string input = "0:";
+    std::string expected_output = "";
+    std::istringstream iss(input);
+    parser.setInput(iss);
+    std::string output = parser.parseString();
+    EXPECT_EQ(output, expected_output);
+}
 
-    TEST(BencodeParserTest, ParseStringTooLongTest) {
-        Bencoding::BencodeParser parser;
-        std::string input = "10:hello";
-        std::istringstream iss(input);
-        parser.setInput(iss);
-        EXPECT_THROW(parser.parseString(), std::length_error);
-    }
+TEST_F(BencodeParserTest, ParseStringInvalidInputTest) {
+    Bencoding::BencodeParser parser;
+    std::string input = "5hello";
+    std::istringstream iss(input);
+    parser.setInput(iss);
+    EXPECT_THROW(parser.parseString(), std::invalid_argument);
+}
+
+TEST_F(BencodeParserTest, ParseStringTooLongTest) {
+    Bencoding::BencodeParser parser;
+    std::string input = "10:hello";
+    std::istringstream iss(input);
+    parser.setInput(iss);
+    EXPECT_THROW(parser.parseString(), std::length_error);
 }
